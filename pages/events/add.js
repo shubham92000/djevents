8bc6cca393815b64ddd2d85ed38ab910d084f937
shubham92000 <sbhsingh616@gmail.com>
2022-04-07 13:05:48 +0530

    changed all imports with aliases

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/Layout';
+import Layout from '@/components/Layout';
 
 const AddEvents = () => {
   return <Layout title={`Add new event`}>AddEvents</Layout>;
