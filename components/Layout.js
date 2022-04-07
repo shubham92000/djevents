58a1fc4da617abbcadddff784138937f091f2c2f
shubham92000 <sbhsingh616@gmail.com>
2022-04-07 13:23:02 +0530

    added hero image in landing page

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
 import styles from '@/styles/Layout.module.css';
+import Showcase from './Showcase';
 
 const Layout = ({ title, description, keywords, children }) => {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -12,6 +15,7 @@ const Layout = ({ title, description, keywords, children }) => {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
