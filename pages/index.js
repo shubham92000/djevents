51fdaddec8cb5112f1eecc47a2338cb415c1b741
shubham92000 <sbhsingh616@gmail.com>
2022-04-08 12:51:14 +0530

    fetch events

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
