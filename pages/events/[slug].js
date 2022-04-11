c0d69f72cf3200ff0d29fc5df19194a7a334474b
shubham92000 <sbhsingh616@gmail.com>
2022-04-11 22:12:08 +0530

    fetch data for individual event in slug page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,45 @@
-import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-const EventPage = () => {
-  return <Layout>EventPage Slug</Layout>;
+const EventPage = ({ evt }) => {
+  return (
+    <Layout>
+      <h1>{evt.name}</h1>
+    </Layout>
+  );
 };
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: { evt: events[0] },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps({ query : { slug } }){
+//   const res = await fetch(`${API_URL}/api/events/${slug}`) ;
+//   const events = await res.json() ;
+
+//   return {
+//     props : { evt : events[0] }
+//   }
+// }
 
 export default EventPage;
